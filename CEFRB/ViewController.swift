d0b0e400fea5fff6f5c0dd49ee3528a08430090a
Andrew Solesa <asolesa@yahoo.ca>
2020-10-16 19:34:24 -0400

    Initial Commit

---
+++ CEFRB/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CEFRB
+//
+//  Created by Andrew Solesa on 2020-10-16.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
